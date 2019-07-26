e5ef190908e455a965ae06d2bf410e1ac6c9a7df
Michael Schott <schottmichaelsw@gmail.com>
2019-07-26 10:06:43 +0200

    Change window add

---
+++ src/main/java/tum/franca/main/AboutWindow.java
@@ -0,0 +1,27 @@
+package tum.franca.main;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AboutWindow {
+	
+	public void showAboutWindow() {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("About.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("About VisualFX Franca");
+			stage.setScene(new Scene(root, 450, 450));
+			stage.setAlwaysOnTop(true);
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
